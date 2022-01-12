c1ffa242bef145bc142cd43ab0371d45537e6faf
CrossCake <97117497+CrossCake@users.noreply.github.com>
2022-01-12 11:35:12 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,7 +179,7 @@
 			.children('.inner')
 				//.css('overflow', 'hidden')
 				.css('overflow-y', browser.mobile ? 'visible' : 'hidden')
-				.css('overflow-x', browser.mobile ? 'scroll' : 'visible')
+				.css('overflow-x', browser.mobile ? 'scroll' : 'hidden')
 				.scrollLeft(0);
 
 		// Style #1.
